9532ac8e291fe83b4c8d8353bda2fe2e3a5cda60
Sa1kxd <samildelacruz1@gmail.com>
2025-09-04 20:36:08 -0400

    Creacion de la clase modelo jugadores

---
+++ RegistrosJugadores/Models/Jugadores.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrosJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+        public int JugadoresId { get; set; }
+        
+        [Required(ErrorMessage = "Este campo es requerido")]
+
+        public String Nombre { get; set; }
+
+        public int Partidas { get; set; }
+
+
+    }
+}
