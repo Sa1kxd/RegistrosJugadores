d7bd50618944b4980e8995aaeae0e5034a953c38
Sa1kxd <samildelacruz1@gmail.com>
2025-09-04 21:16:54 -0400

    Inyeccion del servicio

--- RegistrosJugadores/Program.cs
+++ RegistrosJugadores/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RegistrosJugadores.Components;
 using RegistrosJugadores.DAL;
+using RegistrosJugadores.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,10 @@ builder.Services.AddRazorComponents()
 
 //Obtenemos el ConStr para usarlo en el contexto
 var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
 builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<JugadoresServices>();
 
 var app = builder.Build();
 
