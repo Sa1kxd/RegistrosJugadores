92a6022c3065cbbc29f61d7ed44587cd3d83339f
Sa1kxd <samildelacruz1@gmail.com>
2025-09-04 20:44:32 -0400

    Creacion del contexto y configuracion del ConnectionStrings en el archivo appsettings.json

---
+++ RegistrosJugadores/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosJugadores.Models;
+
+namespace RegistrosJugadores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    
+        public DbSet<Jugadores> Jugadores { get; set; }
+    }
+}
