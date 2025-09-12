a992e59ce83efd08da35407a18a9a4605d8cd0f3
Sa1kxd <samildelacruz1@gmail.com>
2025-09-11 22:40:53 -0400

    Configuracion a la data

--- RegistrosJugadores/Migrations/20250912012820_Inicial.Designer.cs
+++ RegistrosJugadores/Migrations/20250912012820_Inicial.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistrosJugadores.DAL;
 
@@ -11,9 +12,11 @@ using RegistrosJugadores.DAL;
 namespace RegistrosJugadores.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250912012820_Inicial")]
+    partial class Inicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
