3b96809bc184e2324a104e148da3ea26a699f90f
Sa1kxd <163900267+Sa1kxd@users.noreply.github.com>
2025-09-15 20:18:05 -0400

    Update Jugadores.cs
    Añadiendo nuevos atributos a la clase jugador

--- RegistrosJugadores/Models/Jugadores.cs
+++ RegistrosJugadores/Models/Jugadores.cs
@@ -10,4 +10,9 @@ public class Jugadores
     public String Nombre { get; set; }
     [Range(0, int.MaxValue, ErrorMessage = "Valor debe ser mayor a 0")]
     public int Partidas { get; set; }
+public int Victorias {get; set;} = 0;
+
+public int Derrotas {get; set;} = 0;
+
+public int Empates {get; set;} = 0;
 }
