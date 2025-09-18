04a938923d0157cf61a361d1187590d0a0c68e91
Sa1kxd <samildelacruz1@gmail.com>
2025-09-17 23:13:13 -0400

    Renombre de variable Partidas y se agregaron dos variables nuevas a clase jugadores

--- RegistrosJugadores/Models/Jugadores.cs
+++ RegistrosJugadores/Models/Jugadores.cs
@@ -9,5 +9,7 @@ public class Jugadores
     [Required(ErrorMessage = "Este campo es requerido")]
     public String Nombre { get; set; }
     [Range(0, int.MaxValue, ErrorMessage = "Valor debe ser mayor a 0")]
-    public int Partidas { get; set; }
+    public int Victorias { get; set; }
+    public int Derrotas { get; set; }
+    public int Empates { get; set; }
 }
