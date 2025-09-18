0f8f4a3564c400f64af8dab0739630655d5b2b8c
Sa1kxd <samildelacruz1@gmail.com>
2025-09-17 23:15:47 -0400

    Nuevas migraciones

---
+++ RegistrosJugadores/Migrations/20250918031500_ActualizacionJugadores.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistrosJugadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionJugadores : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Partidas",
+                table: "Jugadores",
+                newName: "Victorias");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Derrotas",
+                table: "Jugadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Empates",
+                table: "Jugadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Derrotas",
+                table: "Jugadores");
+
+            migrationBuilder.DropColumn(
+                name: "Empates",
+                table: "Jugadores");
+
+            migrationBuilder.RenameColumn(
+                name: "Victorias",
+                table: "Jugadores",
+                newName: "Partidas");
+        }
+    }
+}
