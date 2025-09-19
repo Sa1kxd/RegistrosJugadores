6eada84156dd14a184f47509e0ad2e3bdf2bf271
Sa1kxd <samildelacruz1@gmail.com>
2025-09-19 18:01:04 -0400

    Realizando nuevas migraciones

---
+++ RegistrosJugadores/Migrations/20250919215738_ActualizacionMovimientos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistrosJugadores.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizacionMovimientos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Jugadas",
+                table: "Jugadores",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Movimientos",
+                columns: table => new
+                {
+                    MovimientoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartidaId = table.Column<int>(type: "int", nullable: false),
+                    JugadorId = table.Column<int>(type: "int", nullable: false),
+                    PosicionFila = table.Column<int>(type: "int", nullable: false),
+                    PosicionColumna = table.Column<int>(type: "int", nullable: false),
+                    FechaMovimiento = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movimientos", x => x.MovimientoId);
+                    table.ForeignKey(
+                        name: "FK_Movimientos_Jugadores_JugadorId",
+                        column: x => x.JugadorId,
+                        principalTable: "Jugadores",
+                        principalColumn: "JugadorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Movimientos_Partidas_PartidaId",
+                        column: x => x.PartidaId,
+                        principalTable: "Partidas",
+                        principalColumn: "PartidaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimientos_JugadorId",
+                table: "Movimientos",
+                column: "JugadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimientos_PartidaId",
+                table: "Movimientos",
+                column: "PartidaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movimientos");
+
+            migrationBuilder.DropColumn(
+                name: "Jugadas",
+                table: "Jugadores");
+        }
+    }
+}
