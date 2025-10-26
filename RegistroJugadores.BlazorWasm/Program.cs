8280d231337174532dd6fbe8c7de60fe867d8007
Sa1kxd <samildelacruz1@gmail.com>
2025-10-26 17:24:40 -0400

    Programs configurado

--- RegistroJugadores.BlazorWasm/Program.cs
+++ RegistroJugadores.BlazorWasm/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RegistroJugadores.BlazorWasm;
+using RegistroJugadores.BlazorWasm.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
+
+builder.Services.AddScoped<IPartidasApiService, PartidasAPIService>();
+builder.Services.AddScoped<IMovimientoAPIService, MovimientoAPIService>();
 
 await builder.Build().RunAsync();
