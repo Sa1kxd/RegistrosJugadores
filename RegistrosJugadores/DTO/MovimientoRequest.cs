a2624d59fab867ed6214e1e6fc42141b637edb2e
Sa1kxd <samildelacruz1@gmail.com>
2025-10-31 11:55:30 -0400

    DTO creados

---
+++ RegistrosJugadores/DTO/MovimientoRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace RegistrosJugadores.DTO;
+
+public class MovimientoRequest
+{
+    public int PartidaId { get; set; }
+
+    public string Jugador { get; set; }
+
+    public int PosicionFila { get; set; }
+
+    public int PosicionColumna { get; set; }
+}
---
+++ RegistrosJugadores/DTO/MovimientoResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace RegistrosJugadores.DTO;
+
+public class MovimientoResponse
+{
+    public int MovimientoId { get; set; }
+    public string Jugador { get; set; }
+
+    public int PosicionFila { get; set; }
+
+    public int PosicionColumna { get; set; }
+}
