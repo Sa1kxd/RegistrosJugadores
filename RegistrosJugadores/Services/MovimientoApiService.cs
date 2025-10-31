5d5ee4fa0955a7ac3632040e40ddff5f0301943f
Sa1kxd <samildelacruz1@gmail.com>
2025-10-31 13:49:17 -0400

    Api services

---
+++ RegistrosJugadores/Services/MovimientoApiService.cs
@@ -0,0 +1,47 @@
+﻿using RegistrosJugadores.DTO;
+
+namespace RegistrosJugadores.Services;
+
+
+public interface IMovimientoApiService
+{
+    Task<Resource<List<MovimientoResponse>>> GetMovimientosAsync(int partidaId);
+    Task<Resource<bool>> PostMovimientosAsync(MovimientoRequest request);
+}
+
+public class MovimientoApiService : IMovimientoApiService
+{
+    private readonly HttpClient _httpClient;
+
+    public MovimientoApiService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<Resource<List<MovimientoResponse>>> GetMovimientosAsync(int partidaId)
+    {
+        try
+        {
+            var result = await _httpClient.GetFromJsonAsync<List<MovimientoResponse>>($"api/Movimientos{partidaId}");
+            return new Resource<List<MovimientoResponse>>.Success(result ?? new());
+        }
+        catch (Exception ex)
+        {
+            return new Resource<List<MovimientoResponse>>.Error(Message: ex.Message);
+        }
+    }
+
+    public async Task<Resource<bool>> PostMovimientosAsync(MovimientoRequest request)
+    {
+        try
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/Movimientos", request);
+            response.EnsureSuccessStatusCode();
+            return new Resource<bool>.Success(true);
+        }
+        catch (Exception ex)
+        {
+            return new Resource<bool>.Error(ex.Message);
+        }
+    }
+}
---
+++ RegistrosJugadores/Services/PartidaApiService.cs
@@ -0,0 +1,26 @@
+﻿using RegistrosJugadores.DTO;
+using System.Net.Http;
+
+namespace RegistrosJugadores.Services;
+
+public interface IPartidaApiService
+{
+    Task<Resource<PartidaResponse>> GetPartidasAsync(int partidaId);
+}
+
+public class PartidaApiService(HttpClient _httpClient) : IPartidaApiService
+{
+    
+   public async Task<Resource<PartidaResponse>> GetPartidasAsync(int partidaId)
+    {
+        try
+        {
+            var response = await _httpClient.GetFromJsonAsync<PartidaResponse>($"api/Partidas/{partidaId}");
+            return new Resource<PartidaResponse>.Success(response!);
+        }
+        catch (Exception ex)
+        {
+            return new Resource<PartidaResponse>.Error(ex.Message);
+        }
+    }
+}
